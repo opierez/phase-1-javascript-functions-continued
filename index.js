2312e5131e8e4d540e3a566cc596750c8405120d
olivia <opierez@gmail.com>
2022-11-21 18:33:02 -0500

    commit tree b8b93be39f1bbf989f06e7df2bde249afbb25959

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(specialChar = "*") {
+    return function (adj = "special") {
+        return `You are ${specialChar}${adj}${specialChar}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
